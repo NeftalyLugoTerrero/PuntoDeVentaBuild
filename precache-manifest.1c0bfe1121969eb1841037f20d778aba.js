9da45e3f5b7029d82ed27d30e2d3f4e8d21d5b20
Neftaly Lugo <20163825@itla.edu.do>
2018-11-01 22:07:25 -0400

    Routes Fixed

---
+++ precache-manifest.1c0bfe1121969eb1841037f20d778aba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "828893a6194ed6a7dc28",
+    "url": "/static/js/main.828893a6.chunk.js"
+  },
+  {
+    "revision": "a93dfc709eca23744442",
+    "url": "/static/js/1.a93dfc70.chunk.js"
+  },
+  {
+    "revision": "828893a6194ed6a7dc28",
+    "url": "/static/css/main.b4179bac.chunk.css"
+  },
+  {
+    "revision": "c64c5c7133e98d481948e7dd574a1531",
+    "url": "/index.html"
+  }
+];
